3614f81ed4ff0f7bd7cc33e549aeaa81cd6e0585
baek0429 <baek0429@hotmail.com>
2015-12-23 10:56:00 +0900

    Learnign Algorithm

--- neuron.go
+++ neuron.go
@@ -1,8 +1,7 @@
 package neural
 
 /* This is go-lang implementation of neural network based on github.com/NOX73/go-neural
-/* I am doing it for fun.
-*/
+ */
 
 /** model structure **/
 type Neuron struct {
@@ -67,5 +66,50 @@ func (e *Enter) SynapseTo(nTo *Neuron, weight float64) {
 func (e *Enter) Signal() {
 	for _, s := range e.OutSynapses {
 		s.Signal(e.Input)
+	}
+}
+
+/* learning algorithm */
+
+type Deltas [][]float64
+
+type Sample struct {
+	In    []float64
+	Ideal []float64
+}
+
+func Learn(n *Network, in, ideal []float64, speed float64) {
+	BackPropagation(n, in, ideal, speed)
+}
+
+func (n *Network) Calculate(in []float64) {
+
+}
+
+func BackPropagation(n *Network, in, ideal []float64, speed float64) {
+	n.Calculate(in)
+
+	deltas := make([][]float64, len(n.Layers)) // 1 by n(layers) matrix
+
+	last := len(n.Layers) - 1 // layer index
+	l := n.Layers[last]
+
+	deltas[last] = make([]float64, len(l.Neurons)) // make detal[i] = neurons of ith layers
+	for i, n := range l.Neurons {
+		deltas[last][i] = n.Out * (1 - n.Out) * (ideal[i] - n.Out)
+	}
+
+	for i := last - 1; i >= 0; i-- {
+		l := n.Layers[i]
+		deltas[i] = make([]float64, len(l.Neurons))
+		for j, n := range l.Neurons {
+
+			var sum float64 = 0
+			for k, s := range n.OutSynapses {
+				sum += s.Weight * deltas[i+1][k]
+			}
+
+			deltas[i][j] = n.Out * (1 - n.Out) * sum
+		}
 	}
 }
